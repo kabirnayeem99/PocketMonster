4702b03805560d2b218c609b7c93c195160d84d3
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-03-06 19:48:27 +0600

    👌 improve: get current user location

--- app/src/main/java/io/github/kabirnayeem99/pocketmonster/MapsActivity.kt
+++ app/src/main/java/io/github/kabirnayeem99/pocketmonster/MapsActivity.kt
@@ -1,9 +1,15 @@
 package io.github.kabirnayeem99.pocketmonster
 
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.location.LocationManager.GPS_PROVIDER
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 
@@ -16,6 +22,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 const val ACCESS_LOCATION_REQUEST_CODE = 1
+const val TAG = "MapsActivity"
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -24,10 +31,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        getUserLocation()
     }
 
     fun checkPermission() {
@@ -38,21 +46,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             return
         }
 
-        getUserLocation()
-
     }
 
     private fun getUserLocation() {
         Toast.makeText(this, "You granted the location permission", Toast.LENGTH_SHORT).show()
-        TODO("Not yet implemented")
+
+        val locationListner = CustomerLocationListener()
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        checkPermission()
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3, 3f, locationListner)
+
+        var locationThread = LocationThread()
+        locationThread.start()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when (requestCode) {
             ACCESS_LOCATION_REQUEST_CODE -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getUserLocation()
-                } else {
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "You didn't grant the permission.", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -64,15 +76,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(
-                MarkerOptions()
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario))
-                        .position(sydney)
-                        .title("Me")
-                        .snippet(" here is my location")
-        )
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18f))
+
+    }
+
+    var assignedLocation: Location? = null
+
+
+    inner class CustomerLocationListener() : LocationListener {
+
+
+        init {
+            assignedLocation = Location(GPS_PROVIDER)
+            assignedLocation!!.longitude = 0.0
+            assignedLocation!!.latitude = 0.0
+        }
+
+        override fun onLocationChanged(location: Location) {
+            assignedLocation = location
+        }
+    }
+
+    inner class LocationThread() : Thread() {
+        override fun run() {
+            while (true) {
+                try {
+                    runOnUiThread {
+                        val sydney = LatLng(assignedLocation!!.latitude, assignedLocation!!.longitude)
+                        mMap.addMarker(
+                                MarkerOptions()
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario))
+                                        .position(sydney)
+                                        .title("Me")
+                                        .snippet(" here is my location")
+                        )
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12f))
+                    }
+
+                    sleep(1000)
+
+                } catch (e: Exception) {
+                    Log.e(TAG, "run: $e")
+                }
+            }
+        }
     }
 }
