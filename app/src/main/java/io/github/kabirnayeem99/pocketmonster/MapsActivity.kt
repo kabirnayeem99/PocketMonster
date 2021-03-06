2ca01bb8254603d48d8eafb02b4490aa04f7ca4a
kabirnayeem99 <39023212+kabirnayeem99@users.noreply.github.com>
2021-03-06 19:14:36 +0600

    👌 improve: implement location permission

--- app/src/main/java/io/github/kabirnayeem99/pocketmonster/MapsActivity.kt
+++ app/src/main/java/io/github/kabirnayeem99/pocketmonster/MapsActivity.kt
@@ -1,14 +1,21 @@
 package io.github.kabirnayeem99.pocketmonster
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+
+const val ACCESS_LOCATION_REQUEST_CODE = 1
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -23,21 +30,49 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    fun checkPermission() {
+        if (Build.VERSION.SDK_INT >= 23 && (ActivityCompat
+                        .checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED)) {
+            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_LOCATION_REQUEST_CODE)
+            return
+        }
+
+        getUserLocation()
+
+    }
+
+    private fun getUserLocation() {
+        Toast.makeText(this, "You granted the location permission", Toast.LENGTH_SHORT).show()
+        TODO("Not yet implemented")
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            ACCESS_LOCATION_REQUEST_CODE -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getUserLocation()
+                } else {
+                    Toast.makeText(this, "You didn't grant the permission.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.addMarker(
+                MarkerOptions()
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario))
+                        .position(sydney)
+                        .title("Me")
+                        .snippet(" here is my location")
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18f))
     }
 }
